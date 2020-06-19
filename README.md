663ee36e5064d3ea88bcede8201c06a39eb3a356
Jonas <uberibsen@gmail.com>
2020-06-19 22:54:46 +0200

    Added coordinate conversion and image export.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # UtilityMapper
-*Currently under development. Stay tuned for updates.*
-This project is focused around the mapping of utility usage from CS:GO demo files.
+*This project is currently under development. Stay tuned for updates.*
+This project is focused around the mapping of utility usage from CS:GO demo files. Feel free to contribute!
 
 ### Planned features:
 - Heatmap generator of utility usage
@@ -8,3 +8,25 @@ This project is focused around the mapping of utility usage from CS:GO demo file
 	- Grenade hits
 	- Effective flashes
 - Export to image files
+
+### Checklist
+-  [x] Demo data extraction using [demofile](https://github.com/saul/demofile)
+	- [x] Grenade coordinates
+	- [x] Grenade damage
+	- [ ] Time flashbanged
+-  [x] JSON parsing from JS
+-  [ ] JSON data extraction in Python
+-  [x] Image export
+- [ ] Multi map support
+	- [x] de_inferno
+	- [ ] de_dust2
+	- [ ] de_train
+	- [ ] de_mirage
+	- [ ] de_overpass
+	- [ ] de_cache
+- [ ] Multi grenade type support
+	- [x] HE Grenade
+	- [ ] Flashbang
+	- [ ] Smoke
+	- [ ] Molotov/Fire
+- [ ] Grenade damage mapping
--- heatmapExport.py
+++ heatmapExport.py
@@ -2,6 +2,9 @@ import os
 import json
 import csv
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 from pandas.io.json import json_normalize
 
 def main():
@@ -14,35 +17,55 @@ def main():
     else:    
         pass
 
-    # Load JSON file
-    with open('json/utility.json', 'r') as utility_json:
-        utilityData = json.load(utility_json)
+    x_coor = []
+    y_coor = []
+    img = plt.imread('maps/de_inferno.png')
 
-    # Flatten JSON dunction
-    def flatten_json(y):
-        output = {}
+    # Opens CSV data file
+    with open ('csv/utility.csv', 'r') as csv_file:
+        csv_data = csv.reader(csv_file, delimiter=',', quotechar='|')
+        for data in csv_data:
+            # Writes data and removes quotations
+            x_coor.append(data[3].strip('"'))
+            y_coor.append(data[4].strip('"'))
 
-        def flatten(x, name=""):
-            if type(x) is dict:
-                for a in x:
-                    flatten(x[a], name + a + '_')
-            elif type(x) is list:
-                i = 0
-                for a in x:
-                    flatten(a, name + str(i) + '_')
-                    i += 1
-            else:
-                output[name[:-1]] = x
-                
-        flatten(y)
-        return output
+    # Removes first unneeded string element
+    del x_coor[0]
+    del y_coor[0]
 
-    # Flatten JSON
-    flat = flatten_json(utilityData)
-    exportArray = pd.json_normalize(flat)
+    # Used to convert absolute x coordinates to for minimap image resolution
+    def pointx_to_resolutionx(xinput, startX = -1960, endX=2797, resX = 1024): # Only works for de_inferno
+        sizeX = endX - startX
+        if startX < 0:
+            xinput += startX *(-1.0)
+        else:
+            xinput += startX
+        xoutput = ((xinput / abs(sizeX)) * resX)
+        return xoutput
 
-    # Exports
-    exportArray.to_csv (r'csv/utility.csv')
+    # Used to convert absolute y coordinates to for minimap image resolution
+    def pointy_to_resolutiony(yinput, startY = -1062, endY = 3800, resY = 1024): # Only works for de_inferno
+        sizeY = endY - startY
+        if startY < 0:
+            yinput += startY *(-1.0)
+        else:
+            yinput += startY
+        youtput = ((yinput / abs(sizeY)) * resY)
+        return resY - youtput
+    
+    # Rounds every element in x_coor to nearest whole and converts coordinate to number in range of the image resolution
+    for i in range (0, len(x_coor)):
+        x_coor[i] = pointx_to_resolutionx(int(float(x_coor[i])))
 
+    # Rounds every element in y_coor to nearest whole and converts coordinate to number in range of the image resolution
+    for i in range (0, len(y_coor)):
+        y_coor[i] = pointy_to_resolutiony(int(float(y_coor[i])))
+
+    # Initiate plots and draw
+    sns.kdeplot(x_coor, y_coor, n_levels = 45, cmap="OrRd", alpha = 0.5, shade=True, shade_lowest=False)
+    sns.scatterplot(x = x_coor, y = y_coor, marker = "o", s = 20, color ="Blue")
+    plt.imshow(img)
+    plt.savefig("export/de_inferno_hegrenade.png", bbox_inches='tight', pad_inches=0)
+    
 if __name__ == '__main__':
     main()
